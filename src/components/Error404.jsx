9e449e20b4c51d02a0d90ca1e1a211fb802c477a
ShayanJa <shay.talebi@gmail.com>
2017-05-02 15:23:53 -0700

    added some user icons

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -9,7 +9,7 @@ export default class Error404 extends React.Component {
         <Header />
             <div id="errorContainer">
                 <div id="error">
-                    <img src={require('../assets/flame.svg')}  width="130" height="130" alt="User avatar, DNA Helix" />
+                    <img src={require('../assets/404Flame.svg')}  width="130" height="130" alt="User avatar, DNA Helix" />
                     <h1 id="errorTitle">404</h1>
                     <div id="errorSlogan">page not found...</div>
                     <div id="errorSlogan">redirecting...</div>
