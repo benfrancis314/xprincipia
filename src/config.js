31251c804b3659af80339bc55b561d9e48c10687
bfrance18@gmail.com <bfrance18gmailcom@bfrance18gmailcoms-MacBook-Pro.local>
2017-09-28 03:53:14 -0500

    changed config location back

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export class Config {
     //TestAPI
-    static API =  'http://ec2-13-58-239-116.us-east-2.compute.amazonaws.com'
+    // static API =  'http://ec2-13-58-239-116.us-east-2.compute.amazonaws.com'
     //LocalHostAPI
     // static API = 'http://localhost:10000'
     //productionAPI
-    // static API = 'https://xprincipiabeta.com'
+    static API = 'https://xprincipiabeta.com'
     //ChatAPI
     static CHATAPI = 'http://ec2-18-221-66-130.us-east-2.compute.amazonaws.com'
 }
