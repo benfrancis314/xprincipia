7745c6b98bd3b0de57e66351bc9002ed061ba8a7
bfrance18@gmail.com <bfrance18gmailcom@bfrance18gmailcoms-MacBook-Pro.local>
2017-10-20 17:06:24 -0500

    switched config back to xprincipiabeta, also removed chat servers since were not using them and they didnt work

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@ export class Config {
         // static API =  'http://ec2-13-58-239-116.us-east-2.compute.amazonaws.com'
         // This is the new one:
         // This DOES work
-    static API =  'http://ec2-18-221-133-185.us-east-2.compute.amazonaws.com'
+    // static API =  'http://ec2-18-221-133-185.us-east-2.compute.amazonaws.com'
     //LocalHostAPI
     // static API = 'http://localhost:10000'
     //productionAPI
-    // static API = 'https://xprincipiabeta.com'
+    static API = 'https://xprincipiabeta.com'
         // Here I'm also going to try the ec2 address for the normal xp_api:
         // It DOESN'T work
         // static API =  'http://ec2-13-59-232-38.us-east-2.compute.amazonaws.com'
